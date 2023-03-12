98dfbaad17684cf3d4a555077df96b94f47b72f6
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-03-12 14:42:12 +0000

    chore: (node/express) serving html as static file and return json response.

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
 const express = require("express");
+const StaticData = require("./static/_data.json");
+const expressInstance = express();
+
+function logger(req) {
+  console.log("[", req.method, "] : ", req.headers.host + req.url);
+}
+
+expressInstance.get("/", function (req, res) {
+  return res.status(200).json(StaticData);
+});
+
+expressInstance.get("/user/:userId", function (req, res) {
+  logger(req);
+  const { userId } = req.params;
+  if (userId == undefined) {
+    return res.status(400).json({ message: " Check your url " });
+  }
+
+  const userInstance = StaticData.find((user) => user.id === parseInt(userId));
+  if (userInstance === undefined)
+    return res.status(404).json({ message: "User not found!" });
+
+  res.status(200).json(userInstance);
+});
+
+expressInstance.listen(8000, function () {
+  console.log("Serving on http://localhost:8000");
+});
+
+/**
+//      JSON RESPONSE.
+serverInstance.get("/", function (req, res) {
+  res.status(200).json([{ name: "susan" }]);
+});
+
+serverInstance.listen(8000, function (req, res) {
+  console.log({ server: "Serving application on http://localhost:8000" });
+});
+ **/
+
+/**
+//      SERVING STATIC FILES AND BASIC EXPRESS.
+const express = require("express");
 const application = express();
 //const { createReadStream, readFile } = require("fs");
 const path = require("path");
@@ -23,3 +66,4 @@ application.all("*", function (req, res) {
 application.listen(5000, () => {
   console.log("Express server listening on port 5000");
 });
+ **/
