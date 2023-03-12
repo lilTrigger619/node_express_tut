33b2e70d03954f7c5c7d198cac5d8f6815ac4808
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-03-12 13:38:23 +0000

    chore: added static files to test serving static files.

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const application = express();
+//const { createReadStream, readFile } = require("fs");
+const path = require("path");
 
-application.get("/",function(req, res){
-  res.write("Homepage!");
-  res.status(200).send();
+application.use(express.static("./static")); // to access static files else it will return 404.
+
+application.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./static/app.html")); //send a file.
+  //  res.status(200).send();
 });
 
-application.get("/about", function(req, res){
+application.get("/about", function (req, res) {
   res.write("About page!");
   res.status(200).send();
 });
 
-application.all("*", function(req, res){
+application.all("*", function (req, res) {
   res.write("Page not found!");
   res.status(404).send();
 });
 
-
-application.listen(5000, ()=>{
+application.listen(5000, () => {
   console.log("Express server listening on port 5000");
-})
+});
